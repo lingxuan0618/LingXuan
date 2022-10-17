34752e26cdc4951e32926df6b64d1f900f20908b
lingxuan <lingxuan0618@gmail.com>
2022-10-17 22:02:04 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <nav class="flex justify-between">
-    <div>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="flex items-center dark:text-white">
+      <img src="@/assets/img/logo.png" alt="Logo">
+      <router-link to="/">關於我</router-link> |
+      <router-link to="/about">休閒活動</router-link> |
+      <router-link to="/about">志工活動</router-link> |
+      <router-link to="/about">大學專題</router-link> |
+      <router-link to="/about">學習歷程</router-link>
     </div>
     <div>
-      <Language/>
+      <!-- <Language/> -->
       <ThemeMode/>
     </div>
   </nav>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
 import ThemeMode from '@/components/ThemeMode.vue';
-import Language from '@/components/Language.vue';
+import Footer from '@/components/Footer.vue';
+// import Language from '@/components/Language.vue';
 
 export default {
   components: {
     ThemeMode,
-    Language,
+    Footer,
+    // Language,
   },
 };
 </script>
@@ -38,11 +45,11 @@ nav {
   padding: 30px;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    // font-weight: bold;
+    // color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #0EA5E9;
     }
   }
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-dark text-white dark:bg-slate-100 dark:text-dark">
+    Copyright© LingXuan
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloFooter',
+};
+</script>
